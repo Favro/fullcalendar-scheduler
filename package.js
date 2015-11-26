6788ddb25d2b5a98ef614782663575fe4a48bd96
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2015-11-26 15:48:01 +0100

    Add meteor packaging

---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  git: 'https://github.com/hansoft/fullcalendar-scheduler',
+  name: 'hansoft:fullcalendar-scheduler',
+  summary: 'A premium add-on to FullCalendar for displaying events and resources.',
+  version: '1.1.0-beta2',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom(['1.0', '1.2']);
+
+  api.use([
+		'jquery',
+		'momentjs:moment@2.4.8',
+		'rzymek:fullcalendar@2.4.0_1',
+  ]);
+
+	api.imply([
+		'rzymek:fullcalendar',
+	]);
+
+	api.addFiles([
+		'dist/scheduler.js',
+		'dist/scheduler.css',
+	]);
+
+});
